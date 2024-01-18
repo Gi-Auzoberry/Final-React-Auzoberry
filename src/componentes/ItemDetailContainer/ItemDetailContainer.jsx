aaf4755d9edd3c9ec448f1488a9c4d6fa0bfc532
Gi Auzoberry <giselle.pd@hotmail.com>
2024-01-18 16:48:20 -0300

    Collection 1

--- src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,19 +1,33 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getProductoIndividual } from "../../asyncmock";
+import { db } from "../../services/config";
+import { getDoc, doc } from "firebase/firestore";
+// import { getProductoIndividual } from "../../asyncmock";
 import ItemDetail from "../ItemDetail/ItemDetail";
 
 const ItemDetailContainer = () => {
 
     const [productoIndividual,setProductoIndividual] = useState(null)
+    const {idItem} = useParams()
     
-    let {idItem} = useParams()
-    idItem = Number(idItem)
-
     useEffect( () => {
+      const nuevoDoc = doc(db, "productos", idItem)
+    
+      getDoc(nuevoDoc)
+        .then( res => {
+          const data = res.data()
+          const nuevoProductoIndividual = {id: res.id, ...data}
+    
+          setProductoIndividual(nuevoProductoIndividual)
+        })
+    }, [idItem])
+    
+    //idItem = Number(idItem)
+    /*useEffect( () => {
         getProductoIndividual(idItem)
             .then(respuesta => setProductoIndividual(respuesta))
     }, [idItem])
+  */
 
   return (
     <div>
@@ -23,3 +37,4 @@ const ItemDetailContainer = () => {
 }
 
 export default ItemDetailContainer
+
--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,27 +1,45 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getProductos, getProductosCategoria } from '../../asyncmock';
+// import { getProductos, getProductosCategoria } from '../../asyncmock';
+import { db } from '../../services/config';
+import { collection, getDocs, where, query } from 'firebase/firestore';
 import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css';
 
 
-const ItemListContainer = ({greeting}) => {
-  
+const ItemListContainer = ({ greeting }) => {
+
   const [productos, setProductos] = useState([])
-  const {idCategoria} = useParams()
-  
-  useEffect (() => {
+  const { idCategoria } = useParams()
+
+  useEffect(() => {
+
+    const verProductos = idCategoria ? query(collection(db, "productos"), where("categoria", "==", idCategoria)) : collection(db, "productos");
+
+    getDocs(verProductos)
+      .then(res => {
+        const nuevoProductos = res.docs.map(item => {
+          const data = item.data();
+          return { id: item.id, ...data }
+        })
+        setProductos(nuevoProductos);
+      })
+  }, [idCategoria])
+
+  /*useEffect (() => {
     const verCategoria= idCategoria ? getProductosCategoria : getProductos
     verCategoria(idCategoria)
       .then(respuesta => setProductos(respuesta))
   }, [idCategoria])
-  
+  */
+
   return (
     <div className='contenedorCards'>
       <h2> {greeting} </h2>
-      <ItemList productos = {productos} />
+      <ItemList productos={productos} />
     </div>
   )
 }
 
 export default ItemListContainer
+
