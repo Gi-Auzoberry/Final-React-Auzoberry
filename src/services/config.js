e61b5ee4999da4a1d2840e4a5439cf4c4a208d6f
Gi Auzoberry <giselle.pd@hotmail.com>
2024-01-19 20:07:27 -0300

    env

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDp_k2_-TjXN7LAbgsbLPc8WuRKZzRY8Bk",
+  apiKey: import.meta.env.VITE_API_KEY_FIREBASE_CONFIG,
   authDomain: "the-petkery-store.firebaseapp.com",
   projectId: "the-petkery-store",
   storageBucket: "the-petkery-store.appspot.com",
