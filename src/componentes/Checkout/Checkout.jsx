704f7a1759cb643fda527a4bd2afdbaa7d3f75c9
Gi Auzoberry <giselle.pd@hotmail.com>
2024-01-19 18:01:50 -0300

    estilos

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -16,6 +16,7 @@ const Checkout = () => {
     const [mailConfirm, setMailConfirm] = useState("")
     const [ordenId, setOrdenId] = useState("")
     const [error, setError] = useState("")
+    const [mostrarOrden, setMostrarOrden] = useState(true)
 
     const handleForm = (e) => {
         e.preventDefault();
@@ -48,6 +49,12 @@ const Checkout = () => {
             .then(docRef => {
                 setOrdenId(docRef.id)
                 vaciarCarrito()
+                setNombre("");
+                setApellido("");
+                setTel("");
+                setMail("");
+                setMailConfirm("");
+                setMostrarOrden(false);
             })
             .catch(error => {
                 setError("No pudimos procesar tu orden, por favor revisa el código")
@@ -58,45 +65,52 @@ const Checkout = () => {
         <>
             <h2> Checkout </h2>
             <form onSubmit={handleForm} className="form-checkout">
-                <div id="orden-de-compra">
-                    {carrito.map(producto => (
-                        <div key={producto.item.id} id="checkout">
-                            <img src={producto.item.img} alt="fotito" />
-                            <p> {producto.item.nombre} x {producto.cantidad} </p>
-                            <p> Precio: $ {producto.item.precio} </p>
-                        </div>
-                    ))}
-                </div>
+                {mostrarOrden && (
+                    <div id="orden-de-compra">
+                        {carrito.map(producto => (
+                            <div key={producto.item.id} id="checkout">
+                                <img src={producto.item.img} alt="fotito" />
+                                <p> {producto.item.nombre} x {producto.cantidad} </p>
+                                <p> Precio: $ {producto.item.precio} </p>
+                            </div>
+                        ))}
+                    </div>
+                )}
 
                 <div id="form-orden">
                     <div className="nombreYapellido">
                         <label htmlFor=""> Nombre: </label>
-                        <input type="text" id="nombre" placeholder="nombre" onChange={(e) => setNombre(e.target.value)} />
+                        <input type="text" id="nombre" placeholder="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                         <label htmlFor=""> Apellido: </label>
-                        <input type="text" id="apellido" placeholder="apellido" onChange={(e) => setApellido(e.target.value)} />
+                        <input type="text" id="apellido" placeholder="apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} />
                     </div>
-                    <div className="tel"> 
+                    <div className="tel">
                         <label htmlFor=""> Teléfono: </label>
-                        <input type="text" id="tel" placeholder="ingresa tu número de teléfono" onChange={(e) => setTel(e.target.value)} />
+                        <input type="text" id="tel" placeholder="ingresa tu número de teléfono" value={tel} onChange={(e) => setTel(e.target.value)} />
                     </div>
                     <div className="mail">
                         <label htmlFor=""> Correo Electónico: </label>
-                        <input type="email" id="mail" placeholder="name@example.com" onChange={(e) => setMail(e.target.value)} />
+                        <input type="email" id="mail" placeholder="name@example.com" value={mail} onChange={(e) => setMail(e.target.value)} />
                     </div>
                     <div className="mailConf">
                         <label htmlFor=""> Confirma tu Correo Electónico: </label>
-                        <input type="email" id="mailConf" placeholder="name@example.com" onChange={(e) => setMailConfirm(e.target.value)} />
+                        <input type="email" id="mailConf" placeholder="name@example.com" value={mailConfirm} onChange={(e) => setMailConfirm(e.target.value)} />
                     </div>
 
                     {error && <h4> {error} </h4>}
 
                     <button type="submit" id="submit"> Finalizar Compra </button>
+               </div>
 
-                    {ordenId && (
-                        <h3> ¡Muchas gracias por tu compra! El número de orden es: {ordenId} </h3>
-                    )}
-                </div>
+
             </form>
+
+            {ordenId && (
+                        <div id="orden-fin">
+                            <img src="../img/gatito cumple.jpeg" alt="Gatito festejando su cumpleaños" id="michi-cumple"/>
+                            <p id="grax"> ¡Muchas gracias por tu compra! <br/> El número de orden es: {ordenId} </p>
+                        </div>
+                    )}
         </>
     )
 }
